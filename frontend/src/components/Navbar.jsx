aaaf07942274a01a9f866c545957208333a33fab
pawaffle <pdegterev@gmail.com>
2024-10-07 10:59:15 +0300

    part 3

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,21 @@
 import { Link } from "react-router-dom";
-import LoadingSpinner from "./LoadingSpinner";
 import { useContext } from "react";
-import ThemeContext from "../context/ThemeContext";
 import AuthContext from "../context/AuthContext";
+import LoadingSpinner from "./LoadingSpinner";
+import ThemeContext from "../context/ThemeContext";
 
 const Navbar = () => {
+  const { isAuthenticated, clearUser, email, isLoading } = useContext(AuthContext);
   const { toggleTheme } = useContext(ThemeContext);
-  const { isAuthenticated, clearUser, email, isLoading } = useContext(AuthContext); // Access Auth
 
-  // const isAuthenticated = false;
-
-  // const toggleTheme = () => {
-    // 
-  // };
   const handleClick = (e) => {
-    clearUser();
+    clearUser(); // Log the user out by clearing their authentication data
   };
 
+  // Show a loading spinner while the authentication state is being checked
   if (isLoading) {
-  return <LoadingSpinner />;
-   }
+    return <LoadingSpinner />;
+  }
 
   return (
     <nav className="navbar">
@@ -27,15 +23,18 @@ const Navbar = () => {
         <h1>React Jobs</h1>
       </Link>
       <div className="links">
-        <div>
-          <Link to="/jobs/add-job">Add Job</Link>
-           {email && <span>{email}</span>} 
-          <button onClick={handleClick}>Log out</button>
-        </div>
-        <div>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </div>
+        {isAuthenticated ? (
+          <div>
+            <Link to="/jobs/add-job">Add Job</Link>
+            {email && <span>{email}</span>}
+            <button onClick={handleClick}>Log out</button>
+          </div>
+        ) : (
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        )}
       </div>
       <button onClick={toggleTheme}>Toggle</button>
     </nav>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
         <input {...email} />
         <label>Password:</label>
         <input {...password} />
-        <button>Sign up</button>
+        <button>Login</button>
       </form>
     </div>
   );
